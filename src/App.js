7081ff26fe9476da1edec02d7d633de9596b8e22
Edward Hurtado <edhur9@gmail.com>
2022-03-19 02:17:31 -0400

    initiated hooks, fetch data , user input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import RecipeItem from "./RecipeItem";
 
 function App() {
+  const [userIngredient, setUserIngredient] = useState("");
+  const [recipeData, setRecipeData] = useState(null);
+  const getRecipeData = async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=8da5a877b22645019ada07ae7bff4422&ingredients=${userIngredient}&number=2`
+    );
+    const data = await response.json();
+    setRecipeData(data);
+    console.log("data here:", recipeData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input
+          type="text"
+          onChange={(e) => setUserIngredient(e.target.value)}
+          required
+          placeholder="Ingredient"
+        ></input>
+        <button onClick={getRecipeData}>Enter</button>
+        {recipeData && <RecipeItem recipeInfo={recipeData} />}
+      </div>
+    </>
   );
 }
 
