7479e580f3a9a08053ac5f7fd1b31143f3107390
Edward Hurtado <edhur9@gmail.com>
2022-03-19 19:08:54 -0400

    modified user input functionality

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import RecipeItem from "./RecipeItem";
 function App() {
   const [userIngredient, setUserIngredient] = useState("");
   const [recipeData, setRecipeData] = useState(null);
+  const [numOfRecipes, setNumOfRecipes] = useState(0);
   const getRecipeData = async () => {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=8da5a877b22645019ada07ae7bff4422&ingredients=${userIngredient}&number=2`
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=8da5a877b22645019ada07ae7bff4422&ingredients=${userIngredient}&number=${numOfRecipes}`
     );
     const data = await response.json();
     setRecipeData(data);
@@ -17,13 +18,28 @@ function App() {
   return (
     <>
       <div className="App">
-        <input
-          type="text"
-          onChange={(e) => setUserIngredient(e.target.value)}
-          required
-          placeholder="Ingredient"
-        ></input>
-        <button onClick={getRecipeData}>Enter</button>
+        <form className="form-control">
+          <input
+            type="text"
+            onChange={(e) => setUserIngredient(e.target.value)}
+            required
+            placeholder="Ingredient"
+          ></input>
+          <select
+            name="number"
+            id="number"
+            onChange={(e) => setNumOfRecipes(e.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <button
+            onClick={getRecipeData}
+            className="icon"
+            type="button"
+          ></button>
+        </form>
         {recipeData && <RecipeItem recipeInfo={recipeData} />}
       </div>
     </>
